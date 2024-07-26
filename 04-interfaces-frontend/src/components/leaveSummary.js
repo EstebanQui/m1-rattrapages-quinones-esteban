125fc2c761297f78b3e1e3f05f681a46d73e792d
EstebanQui <esteban.quinones@outlook.fr>
2024-07-26 15:27:45 +0200

    Update list vacation & leavesummary

--- 04-interfaces-frontend/src/components/leaveSummary.js
+++ 04-interfaces-frontend/src/components/leaveSummary.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const LeaveSummary = () => {
 
-    const totalLeave = 3;
-    const leaveTypes = ["Leave 1", "Leave 2", "Leave 3"];
-    const totalLeft = totalLeave - leaveTypes.length;
+    const totalLeave = 24;
+    const leaveDays = [ 7, 2, 10 ];
+    const totalLeft = totalLeave - leaveDays.length;
 
     return (
         <div className="flex flex-col items-center justify-center">
-            <h1 className="text-3xl font-bold text-center">Vacation summary</h1>
+            <h2 className="text-3xl font-bold text-center">Vacation summary</h2>
             <div className="flex flex-col items-center justify-center mt-4 w-1/2">
                 <div className="flex flex-col items-center justify-center gap-8 w-full">
                     <div className="flex flex-row items-center justify-center w-full">
